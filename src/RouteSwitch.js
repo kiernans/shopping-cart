868b98dd18b78d6414af70377863926bd058c1d0
Kiernan Soriano <ksoriano10@horizon.csueastbay.edu>
2022-07-28 12:36:50 -0700

    Change route from shopping to shop

--- src/RouteSwitch.js
+++ src/RouteSwitch.js
@@ -1,14 +1,14 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Homepage from './Homepage';
-import Shopping from './Shopping';
-import ShoppingCart from './ShoppingCart';
+import Homepage from './components/Homepage';
+import Shopping from './components/Shopping';
+import ShoppingCart from './components/ShoppingCart';
 
 const RouteSwitch = () => {
     return (
         <BrowserRouter>
             <Routes>
                 <Route path="" element={<Homepage />} />
-                <Route path="shopping" element={<Shopping />} />
+                <Route path="shop" element={<Shopping />} />
                 <Route path="cart" element={<ShoppingCart />} />
             </Routes>
         </BrowserRouter>
