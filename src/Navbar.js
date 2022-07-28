a3a8bee444f27c884e81918e9af2b714dbc0f3e9
Kiernan Soriano <ksoriano10@horizon.csueastbay.edu>
2022-07-27 21:58:33 -0700

    Create Navbar component

---
+++ src/Navbar.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Navbar = () => {
+    return (
+        <div className="Navbar">
+            <h1>My Store</h1>
+            <a>Home</a>
+            <a>Shop</a>
+        </div>
+    );
+    
+};
+
+export default Navbar;
