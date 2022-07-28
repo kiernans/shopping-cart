fe6d73ba1b8be62c2ca468105f5f80eaef5c38e1
Kiernan Soriano <ksoriano10@horizon.csueastbay.edu>
2022-07-27 21:58:23 -0700

    Create routes for Shopping and ShoppingCart

---
+++ src/Shopping.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Shopping = () => {
+    return (
+        <h1>Shopping</h1>
+    );
+};
+
+export default Shopping;
---
+++ src/ShoppingCart.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const ShoppingCart = () => {
+    return (
+        <h1>Shopping Cart</h1>
+    );
+};
+
+export default ShoppingCart;
