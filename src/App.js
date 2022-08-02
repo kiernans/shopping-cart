b1edea9b091ea57c5fa48427221738078117496a
Kiernan Soriano <ksoriano10@horizon.csueastbay.edu>
2022-08-02 11:43:59 -0700

    Change RouteSwitch to App

--- src/App.js
+++ src/App.js
@@ -3,16 +3,17 @@ import Homepage from './components/Homepage';
 import Shop from './components/Shop';
 import ShoppingCart from './components/ShoppingCart';
 
-const RouteSwitch = () => {
+const App = () => {
     return (
         <BrowserRouter>
             <Routes>
                 <Route path="" element={<Homepage />} />
                 <Route path="shop" element={<Shop />} />
                 <Route path="cart" element={<ShoppingCart />} />
+                <Route path="*" element={<p>There's nothing here!</p>} />
             </Routes>
         </BrowserRouter>
     );
 };
 
-export default RouteSwitch;
+export default App;
