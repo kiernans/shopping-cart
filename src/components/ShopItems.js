1e8537b52c2f8f947961c350104e023611ed1186
Kiernan Soriano <ksoriano10@horizon.csueastbay.edu>
2022-08-08 16:25:49 -0700

    Import test image to display on ShopItem

---
+++ src/components/ShopItems.js
@@ -0,0 +1,12 @@
+
+const ShopItems = () => {
+    return (
+        <div className="ShopItem">
+            
+            <img src={require=('../1.png')} />
+            
+        </div>
+    );
+};
+
+export default ShopItems;
