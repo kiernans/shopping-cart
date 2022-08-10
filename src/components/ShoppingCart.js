efc7650ee35f8ad40a15c0c7a5eca79cc0659af3
Kiernan Soriano <ksoriano10@horizon.csueastbay.edu>
2022-08-09 20:07:13 -0700

    Display CartItems if they exist

--- src/components/ShoppingCart.js
+++ src/components/ShoppingCart.js
@@ -1,11 +1,23 @@
-import React from "react";
+import React, { useEffect } from "react";
+import CartItem from "./CartItem";
 import "../styles/ShoppingCart.css"
 
-const ShoppingCart = () => {
+const ShoppingCart = ({ total, shopItems, addItem, removeItem }) => {
+    
+    let chosenItems = shopItems.filter(item => item.qty > 0);
+
     return (
         <div className="container">
             <div className="main">
-                <h1>Shopping Cart</h1> 
+                {chosenItems ? chosenItems.map((item) => {
+                                    return <div key={item.id} id={item.id} className='item'>
+                                                <CartItem name={item.name}
+                                                        qty={item.qty}
+                                                        price={item.price} />
+                                            </div>
+                                })
+                            : <p>Your cart is empty</p>
+                }
             </div>
         </div>
         
