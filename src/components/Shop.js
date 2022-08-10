7d950227d6c0a58c65612331e90a474e02e076f7
Kiernan Soriano <ksoriano10@horizon.csueastbay.edu>
2022-08-10 14:11:08 -0700

    Add eventListeners to add/remove buttons

--- src/components/Shop.js
+++ src/components/Shop.js
@@ -1,8 +1,30 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "../styles/Shop.css"
 import ShopItem from "./ShopItem";
 
 const Shop = ({ shopItems, addItem, removeItem }) => {
+
+    useEffect(() => {
+        const addButtons = document.querySelectorAll('.add-item');
+        const removeButtons = document.querySelectorAll('.remove-item');
+        addButtons.forEach(button => {
+            button.addEventListener('click', addItem);
+        });
+        removeButtons.forEach(button => {
+            button.addEventListener('click', removeItem);
+        });
+        
+
+        return () => {
+            addButtons.forEach(button => {
+                button.removeEventListener('click', addItem);
+            })
+            removeButtons.forEach(button => {
+                button.removeEventListener('click', removeItem);
+            })
+        };
+    }, [addItem, removeItem]);
+
     return (
         <div className="container">
             <div className="main">
--- src/components/ShopItem.js
+++ src/components/ShopItem.js
@@ -1,13 +1,16 @@
+import { useEffect } from 'react';
 import '../styles/ShopItem.css';
 
 const ShopItem = ({ name, id, qty, price, addItem }) => {
+
     return (
         <div className="ShopItem" id={id}>
             <img src={require('../1.png')} alt='' />
             <p>{name}</p>
             <p>{qty}</p>
             <p>${price.toFixed(2)}</p>
-            <button onClick={addItem}>Add</button>
+            <button className='add-item' id={id}>Add</button>
+            <button className='remove-item' id={id}>Remove</button>
         </div>
     );
 };
