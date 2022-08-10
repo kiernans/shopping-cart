b2d587520ac8438c791b7bcc8be052f85c796e3a
Kiernan Soriano <ksoriano10@horizon.csueastbay.edu>
2022-08-09 20:05:36 -0700

    Add button to add item

--- src/components/ShopItem.js
+++ src/components/ShopItem.js
@@ -3,10 +3,11 @@ import '../styles/ShopItem.css';
 const ShopItem = ({ name, id, qty, price, addItem }) => {
     return (
         <div className="ShopItem" id={id}>
-            <img src={require('../1.png')} />
+            <img src={require('../1.png')} alt='' />
             <p>{name}</p>
             <p>{qty}</p>
             <p>${price.toFixed(2)}</p>
+            <button onClick={addItem}>Add</button>
         </div>
     );
 };
