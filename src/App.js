321dc64d443b4cb2507e997693cbab3ac2f04057
Kiernan Soriano <ksoriano10@horizon.csueastbay.edu>
2022-08-09 20:04:56 -0700

    Add functions to Shop and Cart components

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Homepage from './components/Homepage';
 import Navbar from './components/Navbar';
 import Shop from './components/Shop';
 import ShoppingCart from './components/ShoppingCart';
-import { getTotal, getItem, incrementQuantity } from './helpers/ShopHelper';
+import { getTotal, getItem, incrementQuantity, decrementQuantity } from './helpers/ShopHelper';
 
 const App = () => {
 
@@ -12,19 +12,19 @@ const App = () => {
         {
             id: '1',
             name: "1",
-            qty: 0,
+            qty: 2,
             price: 1.00
         },
         {
             id: '2',
             name: "2",
-            qty: 0,
+            qty: 1,
             price: 2.00
         },
         {
             id: '3',
             name: "3",
-            qty: 0,
+            qty: 4,
             price: 3.00
         },
         {
@@ -36,7 +36,7 @@ const App = () => {
         {
             id: '5',
             name: "5",
-            qty: 0,
+            qty: 3,
             price: 5.00
         },
         {
@@ -57,14 +57,23 @@ const App = () => {
     const addItem = (e) => {
         setShopItems(incrementQuantity(shopItems, e.target.id));
     };
+    
+    const removeItem = (e) => {
+        setShopItems(decrementQuantity(shopItems, e.target.id));
+    };
 
     return (
         <BrowserRouter>
             <Navbar />
             <Routes>
                 <Route path="" element={<Homepage />} />
-                <Route path="shop" element={<Shop shopItems={shopItems} addItem={addItem}/>} />
-                <Route path="cart" element={<ShoppingCart total={total} />} />
+                <Route path="shop" element={<Shop shopItems={shopItems} 
+                                                  addItem={addItem} 
+                                                  removeItem={removeItem}/>} />
+                <Route path="cart" element={<ShoppingCart total={total} 
+                                                          shopItems={shopItems} 
+                                                          addItem={addItem} 
+                                                          removeItem={removeItem}/>} />
                 <Route path="*" element={<p>There's nothing here!</p>} />
             </Routes>
         </BrowserRouter>
